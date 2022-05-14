01b68a0c05ed9c80ac4b7b2b48047575f0311510
Jaime Pereira <JPszC@pm.me>
2022-05-13 22:47:44 -0400

    Update doom-one.h

--- colors/doom-one.h
+++ colors/doom-one.h
@@ -1,7 +1,7 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#cccccc", "#282c34" },
-	[SchemeSel] = { "#1c1f24", "#c678dd" },
+	[SchemeSel] = { "#1c1f24", "#51afef" },
 	[SchemeSelHighlight] = { "#98be65", "#000000" },
 	[SchemeNormHighlight] = { "#98be65", "#000000" },
 	[SchemeOut] = { "#000000", "#51afef" },
